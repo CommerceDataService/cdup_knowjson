9e063c5665fd51469e1a4749e33d4430a90484a4
Jeff Chen <jchen85@gmail.com>
2016-05-19 00:53:46 -0400

    Header change
    Last edit of the night. Changed titles and hrefs

--- ex.js
+++ ex.js
@@ -6,11 +6,10 @@ var data = $.getJSON("know.json", function(json) {
     console.log(json); // this will show the info it in firebug console
 });
 
-
+//Pushes first 3 articles into the left column at the top of the page
+//Pushes the 4th to a feature section
 $( window ).load(function() {
-     
-
-        
+    
     $('#first_pos').empty();
     $( "#first_pos" ).html("<a href='"+data.responseJSON.data[0][8]+"'<div style='align:justified'><img src='img/"+data.responseJSON.data[0][0]+".jpg' style='float:right; margin: 0 5 0 5' height=60>" + "<h4><strong>"+data.responseJSON.data[0][3]+"</strong></h4></a><p>"+data.responseJSON.data[0][4]+"</p>");
     
@@ -30,14 +29,18 @@ $( window ).load(function() {
 
 
 //Load up datatables from know.json
+
 $(document).ready(function() {
     $('#example').DataTable( {
         
+        //Read in via KNOW.JSON
         "ajax" : {
             "url" : "know.json",
             "dataSrc" : function (json) {
+                
                 console.log(json.data[0][0])
-                //Add img tag to foto label
+                
+                //Add img tag to foto label, loop through each record to populate modals at the bottom of the body
                 for (i = 0; i < json.data.length; i++) { 
                     json.data[i][0] = "<a class='modal-dialog modal-lg' href='#' data-toggle='modal' data-target='#"+i+"'><img src='img/"+json.data[i][0]+".jpg' style='float:left; margin: 0 5 0 5' height=80></a>"
                     $("#begin_modal").append("<div class='modal fade' id='"+i+"' role='dialog'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal'>&times;</button> <h4 class='modal-title'>"+json.data[i][0]+"</h4></div> <div class='modal-body'><p>wha wha</p></div><div class='modal-footer'> <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button></div> </div></div></div>");
@@ -48,7 +51,7 @@ $(document).ready(function() {
                 return json.data;
             }
         },
-        //Remove certain columns
+        //Hide certain columns based on column index
         "columnDefs": [ { "visible": false,  "targets": [1,2,5,6,7] }]
                 
         
